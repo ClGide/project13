1b09c7f3e4285e59cc5b23b5ff89c1d10a02f8e9
ClGide <giderutazihana81@gmail.com>
2023-01-29 12:57:37 +0100

    added division by zero to see if sentry works

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,8 +5,12 @@ from . import views
 from .lettings.urls import letting_patterns
 from .profiles.urls import profile_patterns
 
+def trigger_error(request):
+    return 1/0
+
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('trigger/', trigger_error),
     path("lettings/", include(letting_patterns)),
     path("profiles/", include(profile_patterns)),
     path('admin/', admin.site.urls)
