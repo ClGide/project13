b0c38a4a32914847e748a8ec6884fdc1db235638
ClGide <giderutazihana81@gmail.com>
2023-01-30 16:27:09 +0100

    use capture_message event to see if event is going to be triggered in sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from sentry_sdk import capture_message
 
 from . import views
 from .lettings.urls import letting_patterns
 from .profiles.urls import profile_patterns
 
 
+capture_message("Hello World")  # Will create an event in Sentry.
+
 def trigger_error():
     return 1 / 0
-
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
