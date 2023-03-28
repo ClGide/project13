68784594742f018a80bbe7f11d0a9251ea3409a7
ClGide <giderutazihana81@gmail.com>
2023-03-28 08:41:02 +0200

    little fix

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+from Django.contrib import admin
 
-from .models import Letting
-from .models import Address
-from .models import Profile
-
+from .lettings.models import Letting
+from .lettings.models import Address
+from .profiles.models import Profile
 
 admin.site.register(Letting)
 admin.site.register(Address)
 admin.site.register(Profile)
+
