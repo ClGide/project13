88018f811ff71405219a82d46b4acc0c8d72f6ba
ClGide <giderutazihana81@gmail.com>
2023-05-23 06:46:52 +0200

    trying a new logger

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,6 +13,26 @@ sentry_sdk.init(
     send_default_pii=True
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'sentry_sdk.integrations.django.DjangoIntegration',
+        },
+    },
+    'root': {
+        'handlers': ['sentry'],
+        'level': 'INFO',
+    },
+}
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
